62d15f4e384fd37ffcc9403e234030138d25cc1d
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-10-18 22:21:56 +0200

    Exercicios da aula

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,54 @@
 import React from 'react'
+import { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const [nome, setNome] = useState("")
+  const onChangeName = (event) => {
+    console.log("mudei nome")
+    console.log(event.target.value)
+    setNome(event.target.value)
+  }
+
+  const [idade, setIdade] = useState("")
+  const onChangeIdade = (event) => {
+    console.log("mudei idade")
+    console.log(event.target.value)
+    setIdade(event.target.value)
+  }
+
+  const [email, setEmail] = useState("")
+  const onChangeEmail = (event) => {
+    console.log("mudei email")
+    console.log(event.target.value)
+    setEmail(event.target.value)
+  }
+
+    function checkSend (){
+      console.log("Nome:", nome, "Idade:", idade, "Email:", email)
+      setNome("")
+      setIdade("")
+      setEmail("")
+    }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input type= "text" onChange={onChangeName} value={nome}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input type= "number" onChange={onChangeIdade} value={idade}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input type= "email" onChange={onChangeEmail} value={email}/>
         </label>
-      <button>Enviar dados</button>
+      <button onClick={checkSend}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
